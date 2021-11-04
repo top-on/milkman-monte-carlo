a17d414a9b8897a4eb6021b5ffbd90d75fbb5b0b
Thorben Jensen <6625253+top-on@users.noreply.github.com>
2021-11-04 20:55:07 +0100

    move constants to parameters

--- milkman_monte_carlo/sim.py
+++ milkman_monte_carlo/sim.py
@@ -4,45 +4,47 @@ from typing import List, Tuple
 
 from numpy.random import normal
 
-CALF_N = 5
 
-MILK_MINUTES = 10
-MILK_MINUTES_SD = 2
-
-WALK_MINUTES = 5
-WALK_MINUTES_SD = 2
-
-CALF_MINUTES = 5
-CALF_MINUTES_SD = 2
-
-
-def simulate() -> Tuple[float, float, List[float]]:
+def simulate(
+    calf_n: int,
+    milk_minutes: float,
+    milk_minutes_sd: float,
+    walk_minutes: float,
+    walk_minutes_sd: float,
+    calf_minutes: float,
+    calf_minutes_sd: float,
+) -> Tuple[float, float, List[float]]:
 
     # initialize time counters
     minutes_plan = 0.0
     minutes_real = 0.0
 
     # milk the cow
-    minutes_plan += MILK_MINUTES
-    minutes_real += max(0, MILK_MINUTES + normal(loc=0.0, scale=MILK_MINUTES_SD))
+    minutes_plan += milk_minutes
+    minutes_real += max(0, milk_minutes + normal(loc=0.0, scale=milk_minutes_sd))
 
     minutes_delay = []  # delays when arriving at cows
-    for i in range(CALF_N):
-        # walk to calf
-        minutes_plan += WALK_MINUTES
-        minutes_real += max(0, WALK_MINUTES + normal(loc=0.0, scale=WALK_MINUTES_SD))
+    for i in range(calf_n):
+        # walk to next calf
+        minutes_plan += walk_minutes
+        minutes_real += max(0, walk_minutes + normal(loc=0.0, scale=walk_minutes_sd))
         minutes_delay.append(minutes_real - minutes_plan)
 
-        # feed a calf
-        minutes_plan += CALF_MINUTES
-        minutes_real += max(0, CALF_MINUTES + normal(loc=0.0, scale=CALF_MINUTES_SD))
+        # give milk to calf
+        minutes_plan += calf_minutes
+        minutes_real += max(0, calf_minutes + normal(loc=0.0, scale=calf_minutes_sd))
 
     return minutes_plan, minutes_real, minutes_delay
 
 
-normal()
-
-
 if __name__ == "__main__":
-    minutes_plan, minutes_real, minutes_delay = simulate()
+    minutes_plan, minutes_real, minutes_delay = simulate(
+        calf_n=5,
+        milk_minutes=10.0,
+        milk_minutes_sd=2.0,
+        walk_minutes=5.0,
+        walk_minutes_sd=2.0,
+        calf_minutes=5.0,
+        calf_minutes_sd=2.0,
+    )
     print(f"{minutes_plan = }; {minutes_real = }; {minutes_delay = }")
